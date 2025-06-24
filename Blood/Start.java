276d7cce01231a3501901379f03bae8d999afcfd
Abdullah212-create <abdullahevneyhossain@gmail.com>
2025-06-24 16:29:19 +0600

    Add files via upload

---
+++ Blood/Start.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+import javax.swing.*;
+import Entities.*;
+import Frames.*;
+
+public class Start{
+	public static void main( String[] args){
+		FrameIntro f1=new FrameIntro();
+		f1.setVisible(true);
+		//-Homepage hm1=new Homepage();
+				//this.setVisible(false);
+				//hm1.setVisible(true);
+				//AdminPanel a1=new AdminPanel();
+				//a1.setVisible(true);
+	}
+}
